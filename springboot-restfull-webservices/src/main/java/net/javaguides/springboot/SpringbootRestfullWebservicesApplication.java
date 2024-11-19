bc7d9541abaa99c6c5a19e75aa39fb92e5a3f0f8
Pradeep92hv <hvpradeep92@gmail.com>
2024-11-19 19:11:05 +0530

    ignore

--- springboot-restfull-webservices/src/main/java/net/javaguides/springboot/SpringbootRestfullWebservicesApplication.java
+++ springboot-restfull-webservices/src/main/java/net/javaguides/springboot/SpringbootRestfullWebservicesApplication.java
@@ -9,5 +9,5 @@ public class SpringbootRestfullWebservicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootRestfullWebservicesApplication.class, args);
 	}
-
+//
 }
