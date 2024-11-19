a25d984bd814dc50fadd649b7d19983994ed5ccf
Pradeep92hv <hvpradeep92@gmail.com>
2024-11-19 14:23:49 +0530

    Your commit message here

---
+++ springboot-rest-api/src/main/java/net/javaguides/springboot_rest_api/SpringbootRestApiApplication.java
@@ -0,0 +1,15 @@
+package net.javaguides.springboot_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestApiApplication.class, args);
+		System.out.println("hello");
+		//fhgfddzxfcghjmk
+	}
+
+}
