21a9fbf271e6d4932e96d02db661a8e6d78403fe
Pradeep92hv <hvpradeep92@gmail.com>
2024-11-19 16:43:21 +0530

    create spring boot REST API that with Request variable

--- springboot-rest-api/src/main/java/net/javaguides/springboot_rest_api/controller/StudentController.java
+++ springboot-rest-api/src/main/java/net/javaguides/springboot_rest_api/controller/StudentController.java
@@ -3,6 +3,7 @@ package net.javaguides.springboot_rest_api.controller;
 import net.javaguides.springboot_rest_api.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -38,11 +39,21 @@ public class StudentController {
 
     // spring boot REST API that with path variable
 
-    //http://localhost:8080/students/1
+    //http://localhost:8080/students/1/jyfh/try
     @GetMapping("students/{id}/{first-name}/{last-name}")
     public Student studentPathVariable(@PathVariable("id") int studentId,
                                        @PathVariable("first-name") String firstNmae,
                                        @PathVariable("last-name") String lastname){
         return new Student(studentId,firstNmae,lastname);
+    }
+
+    // create spring boot REST API that with Request variable , it extract the value from query paramter,  path varivale use value from url
+
+    //http://localhost:8080/students/query?id=1&firstName=pradeep&lastName=HV
+    @GetMapping("students/query")
+    public Student studentRequestVariable(@RequestParam int id,
+                                          String firstName,
+                                          String lastName){
+        return new Student(id,firstName,lastName);
     }
 }
