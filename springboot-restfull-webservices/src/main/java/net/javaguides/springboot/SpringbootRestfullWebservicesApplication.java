db12db397cea0efa07018d488721abbe4f362188
Pradeep92hv <hvpradeep92@gmail.com>
2024-11-19 20:07:55 +0530

    21. Configure MySQL Database in Spring Boot App

--- springboot-restfull-webservices/src/main/java/net/javaguides/springboot/SpringbootRestfullWebservicesApplication.java
+++ springboot-restfull-webservices/src/main/java/net/javaguides/springboot/SpringbootRestfullWebservicesApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootRestfullWebservicesApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringbootRestfullWebservicesApplication.class, args);
+		System.out.println("hello");
 	}
-//
+
 }
