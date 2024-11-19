f39724627c7a6b83380c8af391444b020e5cc8a2
Pradeep92hv <hvpradeep92@gmail.com>
2024-11-19 21:00:32 +0530

    22. Create User JPA Entity

---
+++ springboot-restfull-webservices/src/main/java/net/javaguides/springboot/entity/User.java
@@ -0,0 +1,27 @@
+package net.javaguides.springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+// from lombak we cna use getter,setter,arg notation
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity  // to make it enitity
+@Table(name="users")   // to give table details like name
+public class User {
+
+    @Id   // to  make primary key
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    @Column(name = "first_name",nullable = false)
+    String firstName;
+    @Column(nullable = false)
+    String lastName;
+    @Column(nullable = false,unique = true  )
+    String email;
+}
