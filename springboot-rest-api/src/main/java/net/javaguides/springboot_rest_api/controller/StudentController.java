0d6898264aafbfe7cc5f1f9bc551f3b855efa06b
Pradeep92hv <hvpradeep92@gmail.com>
2024-11-19 16:24:11 +0530

    REST API that returns list (list of studnet object as json)

--- springboot-rest-api/src/main/java/net/javaguides/springboot_rest_api/controller/StudentController.java
+++ springboot-rest-api/src/main/java/net/javaguides/springboot_rest_api/controller/StudentController.java
@@ -4,6 +4,9 @@ import net.javaguides.springboot_rest_api.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
@@ -17,5 +20,18 @@ public class StudentController {
         );
 
         return  student;
+    }
+
+
+    //http://localhost:8080/students
+@GetMapping("students")
+    public List<Student> getstudents(){
+
+        List<Student> students=new ArrayList<>();
+
+        students.add(new Student(1,"a","b"));
+        students.add(new Student(2,"aa","bb"));
+
+        return  students;
     }
 }
