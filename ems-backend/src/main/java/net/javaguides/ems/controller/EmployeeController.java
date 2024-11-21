ba55da995e0daa97bd7dacc42b832b76f58d6ec7
Pradeep92hv <hvpradeep92@gmail.com>
2024-11-21 14:13:29 +0530

    Add employee REST API

---
+++ ems-backend/src/main/java/net/javaguides/ems/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package net.javaguides.ems.controller;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.ems.dto.EmployeeDto;
+import net.javaguides.ems.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/employees")
+@AllArgsConstructor
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto)
+    {
+       EmployeeDto savedEmployeeDto= employeeService.createEmployee(employeeDto);
+       return  new ResponseEntity<>(savedEmployeeDto, HttpStatus.CREATED);
+    }
+
+}
