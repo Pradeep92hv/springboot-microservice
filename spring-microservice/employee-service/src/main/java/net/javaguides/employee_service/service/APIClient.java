88fc0124cf9274915e276b040d44938673171795
Pradeep92hv <hvpradeep92@gmail.com>
2024-11-22 19:24:38 +0530

    109. Microservices Communication using Spring Cloud OpenFeign

---
+++ spring-microservice/employee-service/src/main/java/net/javaguides/employee_service/service/APIClient.java
@@ -0,0 +1,18 @@
+package net.javaguides.employee_service.service;
+
+import net.javaguides.employee_service.dto.DepartmentDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(url = "http://localhost:8080/",value = "DEPARTMENT-SERVICE")
+public interface APIClient {
+
+    @GetMapping("api/departments/{department-code}")
+    DepartmentDto getDepartment(@PathVariable("department-code") String departmentCode);
+
+}
+
+// OpenFeign lib dynamically write implementation for interface
--- spring-microservice/employee-service/src/main/java/net/javaguides/employee_service/service/impl/EmployeeServiceImpl.java
+++ spring-microservice/employee-service/src/main/java/net/javaguides/employee_service/service/impl/EmployeeServiceImpl.java
@@ -7,10 +7,12 @@ import net.javaguides.employee_service.dto.EmployeeDto;
 import net.javaguides.employee_service.entity.Employee;
 import net.javaguides.employee_service.mapper.EmployeeMapper;
 import net.javaguides.employee_service.repository.EmployeeRepository;
+import net.javaguides.employee_service.service.APIClient;
 import net.javaguides.employee_service.service.EmployeeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
@@ -18,8 +20,11 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
 
-    private RestTemplate restTemplate;
+//    private RestTemplate restTemplate;
 
+//    private WebClient webClient;
+
+    private APIClient apiClient;
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
@@ -36,9 +41,18 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         Employee employee=employeeRepository.findById(employeeId).get();
 
-        ResponseEntity<DepartmentDto> responseEntity= restTemplate.getForEntity("http://localhost:8080/api/departments/"+employee.getDepartmentCode(), DepartmentDto.class);
+        // for RestTemplate
+//        ResponseEntity<DepartmentDto> responseEntity= restTemplate.getForEntity("http://localhost:8080/api/departments/"+employee.getDepartmentCode(), DepartmentDto.class);
+//
+//        DepartmentDto departmentDto=responseEntity.getBody();
 
-        DepartmentDto departmentDto=responseEntity.getBody();
+        // for webClient
+//       DepartmentDto departmentDto= webClient.get().uri("http://localhost:8080/api/departments/"+employee.getDepartmentCode())
+//                .retrieve()
+//                .bodyToMono(DepartmentDto.class)
+//                .block();
+
+        DepartmentDto departmentDto=  apiClient.getDepartment(employee.getDepartmentCode());
 
         EmployeeDto employeeDto=EmployeeMapper.mapToEmployeeDto(employee);
 
