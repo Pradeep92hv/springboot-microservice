f24858e717f0a23fdd04334604b802e57eeac402
Pradeep92hv <hvpradeep92@gmail.com>
2024-11-22 16:08:41 +0530

    102. EmployeeService - Create Employee JPA Entity and Spring Data JPA Repository

---
+++ spring-microservice/employee-service/src/main/java/net/javaguides/employee_service/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package net.javaguides.employee_service.service;
+
+
+import net.javaguides.employee_service.dto.EmployeeDto;
+
+public interface EmployeeService {
+    EmployeeDto saveEmployee(EmployeeDto employeeDto);
+
+
+}
